cd426b0b791993f82cacd0f3af44c1e302352e1a
Aram Hammoudeh <aram.hammoudeh@indr.com>
2025-01-04 15:40:47 -0700

    move rust to root

--- goud-compressor/build.sh
+++
@@ -1,2 +0,0 @@
-# Run the wasm-pack build command
-wasm-pack build --target bundler --out-dir ts-wrapper/wasm
--- ts-wrapper/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as wasm from "../wasm/goud_compressor";
-
-export async function compress(input: Uint8Array): Promise<Uint8Array> {
-    return wasm.compress(input);
-}
-
-export async function decompress(input: Uint8Array): Promise<Uint8Array> {
-    return wasm.decompress(input);
-}
